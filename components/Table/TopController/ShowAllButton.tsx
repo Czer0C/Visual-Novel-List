5f71fe3487e67181f1201eb251e0c5bf583b133e
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2021-04-01 22:41:27 +0700

    color update

--- components/Table/TopController/ShowAllButton.tsx
+++ components/Table/TopController/ShowAllButton.tsx
@@ -8,18 +8,20 @@ interface ShowAllButtonProps {
 }
 
 export const ShowAllButton = ({ displayAll, showAll, disabled }: ShowAllButtonProps) => {
-  console.log(`full list, `, {disabled})
+  console.log(`full list, `, { disabled })
   return (
     <Button
       className={`
-            flex-shrink-0 px-4 py-2.5 font-semibold ml-12
-          text-white rounded-lg shadow-lg text-center transform 
+            flex-shrink-0 px-4 py-2.5 font-semibold ml-12 
+            focus:outline-none focus:ring-2 focus:ring-indigo-500 
+            focus:ring-offset-2 focus:ring-offset-indigo-200
+            text-white rounded-lg shadow-lg text-center transform 
              transition duration-200 ease-in-out font-sans text-lg
             
             ${disabled && 'cursor-not-allowed bg-indigo-400'}
-            ${!disabled && showAll ? 
-              "bg-indigo-700 hover:bg-indigo-400 hover:translate-y-1 " : 
-              "bg-indigo-400 hover:bg-indigo-700 hover:translate-y-1 "}
+            ${!disabled && showAll ?
+          "bg-indigo-700 hover:bg-indigo-400 hover:translate-y-1 " :
+          "bg-indigo-400 hover:bg-indigo-700 hover:translate-y-1 "}
             
             
             `}
