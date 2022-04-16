25d99c1d7e15d353f006f8cf465d52ae15e55340
TrieuNH7 <TrieuNH7@fpt.com.vn>
2022-04-16 20:03:11 +0700

    theeme bbutton

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import * as TogglePrimitive from '@radix-ui/react-toggle';
 
-import Button from '@/components/Button';
+import Tooltip from '@/components/Tooltip';
 
 const Toggle = () => {
   const [isDark, toggleDark] = useState<boolean>(false);
@@ -35,23 +34,97 @@ const Toggle = () => {
     }
   };
 
+  const color = isDark ? '#fff' : '#000';
+  const maskColor = isDark ? '#000' : '#fff';
+
   return (
-    <TogglePrimitive.Root
-      pressed={isDark}
-      onPressedChange={handleToggle}
-      asChild
-    >
-      <Button>
-        {isDark ? (
-          <MoonIcon className="h-5 w-5 text-gray-300 dark:text-gray-200" />
-        ) : (
-          <SunIcon className="h-5 w-5 text-gray-700 dark:text-gray-300" />
-        )}
-        <span className="ml-2 leading-5 dark:text-gray-200 ">
-          {isDark ? 'Dark' : 'Light'}
-        </span>
-      </Button>
-    </TogglePrimitive.Root>
+    <Tooltip content={isDark ? 'Light Mode' : 'Dark Mode'}>
+      <TogglePrimitive.Root
+        pressed={isDark}
+        onPressedChange={handleToggle}
+        asChild
+      >
+        <button
+          className="theme-button"
+          type="button"
+          aria-label="Toggle Dark Mode"
+        >
+          <div className="moon-or-sun" />
+          <div className="moon-mask" />
+          <style jsx>{`
+            .theme-button {
+              opacity: 0.5;
+              position: relative;
+              border-radius: 5px;
+              width: 42px;
+              height: 42px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              transition: opacity 0.3s ease;
+            }
+            .theme-button:hover {
+              opacity: 1;
+            }
+            .moon-or-sun {
+              position: relative;
+              width: 20px;
+              height: 20px;
+              border-radius: 50%;
+              border: ${isDark ? '4px' : '2px'} solid;
+              border-color: ${color};
+              background: ${color};
+              transform: scale(${isDark ? 0.5 : 1});
+              transition: all 0.45s ease;
+              overflow: ${isDark ? 'visible' : 'hidden'};
+            }
+            .moon-or-sun::before {
+              content: '';
+              position: absolute;
+              right: -9px;
+              top: -9px;
+              height: 20px;
+              width: 20px;
+              border: 2px solid;
+              border-color: ${color};
+              border-radius: 50%;
+              transform: translate(${isDark ? '14px, -14px' : '0, 0'});
+              opacity: ${isDark ? 0 : 1};
+              transition: transform 0.45s ease;
+            }
+            .moon-or-sun::after {
+              content: '';
+              width: 8px;
+              height: 8px;
+              border-radius: 50%;
+              margin: -4px 0 0 -4px;
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              box-shadow: 0 -23px 0 ${color}, 0 23px 0 ${color},
+                23px 0 0 ${color}, -23px 0 0 ${color}, 15px 15px 0 ${color},
+                -15px 15px 0 ${color}, 15px -15px 0 ${color},
+                -15px -15px 0 ${color};
+              transform: scale(${isDark ? 1 : 0});
+              transition: all 0.35s ease;
+            }
+            .moon-mask {
+              position: absolute;
+              right: 4px;
+              top: 4px;
+              height: 20px;
+              width: 20px;
+              border-radius: 50%;
+              border: 0;
+              background: ${maskColor};
+              transform: translate(${isDark ? '4px, -4px' : '0, 0'});
+              opacity: ${isDark ? 0 : 1};
+              transition: transform 0.45s ease;
+            }
+          `}</style>
+        </button>
+      </TogglePrimitive.Root>
+    </Tooltip>
   );
 };
 
